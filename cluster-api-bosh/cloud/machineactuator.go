4fb0d6bceeaea4b311197183836d488231ad2746
Meaghan Kjelland <mkjelland@google.com>
2018-01-05 14:44:00 -0800

    Remove references to cloud provider in BOSH cluter-api

--- cluster-api-bosh/cloud/machineactuator.go
+++ cluster-api-bosh/cloud/machineactuator.go
@@ -35,8 +35,8 @@ type MachineActuator interface {
 
 	// Create and start the machine controller. The list of initial
 	// machines don't have to be reconciled as part of this function, but
-	// are provided in case the function wants to refer to them (and their
-	// ProviderConfigs) to know how to configure the machine controller.
+	// are provided in case the function wants to refer to them to know
+	// how to configure the machine controller.
 	// Not idempotent.
 	CreateMachineController(cluster *clusterv1.Cluster, initialMachines []*clusterv1.Machine) error
 	PostDelete(cluster *clusterv1.Cluster, machines []*clusterv1.Machine) error
