865bfb07aa8a9fb532a252713c757369cc594524
Meaghan Kjelland <mkjelland@google.com>
2018-01-31 10:10:09 -0800

    Fix yaml parsing

--- cluster-api-bosh/cloud/bosh/kubo/generator.go
+++ cluster-api-bosh/cloud/bosh/kubo/generator.go
@@ -33,118 +33,117 @@ type ManifestGenerator struct{}
 
 const kubo_1_9_2 = `
 ---
-- azs:
-  - z1
-  instances: 1
-  jobs:
-  - name: secure-var-vcap
-    release: kubo-1.9.2
-  - name: flanneld
-    release: kubo-1.9.2
-  - name: docker
-    properties:
-      bip: 172.17.0.1/24
-      default_ulimits:
-      - nofile=65536
-      env: {}
-      flannel: true
-      ip_masq: false
-      iptables: false
-      log_level: error
-      storage_driver: overlay
-      store_dir: /var/vcap/data
-      tls_cacert: ((tls-docker.ca))
-      tls_cert: ((tls-docker.certificate))
-      tls_key: ((tls-docker.private_key))
-    release: docker
-  - name: cloud-provider
-    properties:
-      cloud-provider:
-        type: gce
-    provides:
-      cloud-provider:
-        as: worker
-    release: kubo-1.9.2
-  - name: kubelet
-    properties:
-      api-token: ((kubelet-password))
-      tls:
-        kubelet: ((tls-kubelet))
-        kubernetes: ((tls-kubernetes))
-    release: kubo-1.9.2
-  - name: kube-proxy
-    properties:
-      api-token: ((kube-proxy-password))
-      tls:
-        kubernetes: ((tls-kubernetes))
-    release: kubo-1.9.2
-  name: worker
-  networks:
-  - name: default
-  persistent_disk: 10240
-  stemcell: trusty
-  vm_type: worker
+azs:
+- z1
+instances: 1
+jobs:
+- name: secure-var-vcap
+  release: kubo-1.9.2
+- name: flanneld
+  release: kubo-1.9.2
+- name: docker
+  properties:
+    bip: 172.17.0.1/24
+    default_ulimits:
+    - nofile=65536
+    env: {}
+    flannel: true
+    ip_masq: false
+    iptables: false
+    log_level: error
+    storage_driver: overlay
+    store_dir: /var/vcap/data
+    tls_cacert: ((tls-docker.ca))
+    tls_cert: ((tls-docker.certificate))
+    tls_key: ((tls-docker.private_key))
+  release: docker
+- name: cloud-provider
+  properties:
+    cloud-provider:
+      type: gce
+  provides:
+    cloud-provider:
+      as: worker
+  release: kubo-1.9.2
+- name: kubelet
+  properties:
+    api-token: ((kubelet-password))
+    tls:
+      kubelet: ((tls-kubelet))
+      kubernetes: ((tls-kubernetes))
+  release: kubo-1.9.2
+- name: kube-proxy
+  properties:
+    api-token: ((kube-proxy-password))
+    tls:
+      kubernetes: ((tls-kubernetes))
+  release: kubo-1.9.2
+name: worker
+networks:
+- name: default
+persistent_disk: 10240
+stemcell: trusty
+vm_type: worker
 `
 
 const kubo_1_8_6 = `
 ---
-- azs:
-  - z1
-  instances: 1
-  jobs:
-  - name: secure-var-vcap
-    release: kubo-1.8.6
-  - name: flanneld
-    release: kubo-1.8.6
-  - name: docker
-    properties:
-      bip: 172.17.0.1/24
-      default_ulimits:
-      - nofile=65536
-      env: {}
-      flannel: true
-      ip_masq: false
-      iptables: false
-      log_level: error
-      storage_driver: overlay
-      store_dir: /var/vcap/data
-      tls_cacert: ((tls-docker.ca))
-      tls_cert: ((tls-docker.certificate))
-      tls_key: ((tls-docker.private_key))
-    release: docker
-  - name: cloud-provider
-    properties:
-      cloud-provider:
-        type: gce
-    provides:
-      cloud-provider:
-        as: worker
-    release: kubo-1.8.6
-  - name: kubelet
-    properties:
-      api-token: ((kubelet-password))
-      tls:
-        kubelet: ((tls-kubelet))
-        kubernetes: ((tls-kubernetes))
-    release: kubo-1.8.6
-  - name: kube-proxy
-    properties:
-      api-token: ((kube-proxy-password))
-      tls:
-        kubernetes: ((tls-kubernetes))
-    release: kubo-1.8.6
-  name: worker
-  networks:
-  - name: default
-  persistent_disk: 10240
-  stemcell: trusty
-  vm_type: worker
+azs:
+- z1
+instances: 1
+jobs:
+- name: secure-var-vcap
+  release: kubo-1.8.6
+- name: flanneld
+  release: kubo-1.8.6
+- name: docker
+  properties:
+    bip: 172.17.0.1/24
+    default_ulimits:
+    - nofile=65536
+    env: {}
+    flannel: true
+    ip_masq: false
+    iptables: false
+    log_level: error
+    storage_driver: overlay
+    store_dir: /var/vcap/data
+    tls_cacert: ((tls-docker.ca))
+    tls_cert: ((tls-docker.certificate))
+    tls_key: ((tls-docker.private_key))
+  release: docker
+- name: cloud-provider
+  properties:
+    cloud-provider:
+      type: gce
+  provides:
+    cloud-provider:
+      as: worker
+  release: kubo-1.8.6
+- name: kubelet
+  properties:
+    api-token: ((kubelet-password))
+    tls:
+      kubelet: ((tls-kubelet))
+      kubernetes: ((tls-kubernetes))
+  release: kubo-1.8.6
+- name: kube-proxy
+  properties:
+    api-token: ((kube-proxy-password))
+    tls:
+      kubernetes: ((tls-kubernetes))
+  release: kubo-1.8.6
+name: worker
+networks:
+- name: default
+persistent_disk: 10240
+stemcell: trusty
+vm_type: worker
 `
-var workerInstanceGroups map[string]director.InstanceGroup{}
-
-var releases map[string]director.Release{}
+var workerInstanceGroups map[string]director.InstanceGroup
 
 func init() {
+	workerInstanceGroups = map[string]director.InstanceGroup{}
 	instanceGroup := director.InstanceGroup{}
 	if err := yaml.Unmarshal([]byte(kubo_1_9_2), &instanceGroup); err != nil {
 		panic(fmt.Errorf("unmarshalling kubo 1.9.2: %v", err))
